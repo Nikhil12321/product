a6935f9d868f59ae9100c301965760d0bfaf41e2
Nikhil12321 <bhatia.nikhil95@gmail.com>
2017-09-16 23:26:08 +0530

    corrected some drawables

--- app/src/main/res/drawable/wisdom_mcb_change.py
+++
@@ -1,9 +0,0 @@
-'''
-
-import os
-for filename in os.listdir("."):
-	os.rename(filename, filename.replace("-", "_")
-'''
-import os
-for filename in os.listdir("."):
-	os.rename(filename, "wisdom_"+filename)
